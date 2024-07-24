345f8884838257e435ee8dcda4988f90477de03a
William <172239352+willi-felix@users.noreply.github.com>
2024-07-24 22:06:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ def calculate_bmi(weight, height_cm):
 
 def get_bmi_category(bmi):
     if bmi < 18.5:
-        return ```You are underweight
-        You need a more reasonable diet```
+        return """You are underweight
+        You need a more reasonable diet"""
     elif 18.5 <= bmi <= 24.9:
-        return ```Your body is normal
-        You are a person with a reasonable diet```
+        return """Your body is normal
+        You are a person with a reasonable diet"""
     elif 25 <= bmi <= 29.9:
-        return ```You are an overweight person
-        You need to fix just a few parts of your diet```
+        return """You are an overweight person
+        You need to fix just a few parts of your diet"""
     elif >= 30:
-        return ```You are an obese person
-        You need to immediately correct your diet```
+        return """You are an obese person
+        You need to immediately correct your diet"""
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
